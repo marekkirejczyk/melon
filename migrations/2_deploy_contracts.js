7366cc46a5324eaf64508b0de83b4e7180995501
Marek <marek.kirejczyk@gmail.com>
2017-05-08 14:00:22 +0200

    fix migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,12 @@
+// To get melonToken address
+// let contribution = "..." // e.g. 0x7bc2cc74bceb99c913586fb6f8a6941572662af2
+// Contribution.at(contribution).then((c) => c.melonToken()).then((a) => console.log(a))
+
 module.exports = (deployer) => {
   const melonport = '0x32cd3282d33ff58b4ae8402a226a0b27441b7f1a';
   const signer = '0x32cd3282d33ff58b4ae8402a226a0b27441b7f1a';
   const btcs = '0x32cd3282d33ff58b4ae8402a226a0b27441b7f1b';
-  const startTime = 1483839675;
+  const startTime = 1494154343;
+  
   deployer.deploy(Contribution, melonport, btcs, signer, startTime);
 };
